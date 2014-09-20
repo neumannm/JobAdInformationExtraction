23e2ed372de818f7562393edf218f5c8d3fa343f
"Mandy Neumann" <mandyneumann@gmx.net>
2014-09-20 16:34:33 +0200

    ...

--- MasterArbeitProjekt/src/spinfo/tm/extraction/parsing/DepCompetenceFinderWorkflow.java
+++ MasterArbeitProjekt/src/spinfo/tm/extraction/parsing/DepCompetenceFinderWorkflow.java
@@ -1,8 +1,12 @@
 package spinfo.tm.extraction.parsing;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +16,7 @@ import spinfo.tm.data.Paragraph;
 import spinfo.tm.extraction.data.Class;
 import spinfo.tm.extraction.data.SlotFiller;
 import spinfo.tm.util.ClassFilter;
+import spinfo.tm.util.IETrainingDataGenerator;
 import spinfo.tm.util.ReaderWriter;
 
 public class DepCompetenceFinderWorkflow {
@@ -19,6 +24,7 @@ public class DepCompetenceFinderWorkflow {
 	private static final String TRAININGDATAFILE = "data/SingleClassTrainingDataFiltered.csv";
 	private static final String IE_TRAININGDATAFILE = "data/trainingIE_140816.csv";
 	private static final String PARSEDPARAGRAPHSFILE = "data/parsedParagraphs.bin";
+	private static final String VERBSOFINTERESTFILE = "models/verbsOfInterest.txt";
 	private static Logger logger;
 
 	public static void main(String[] args) {
@@ -34,7 +40,7 @@ public class DepCompetenceFinderWorkflow {
 				.readParagraphsFromBinary(parsedParagraphsFile);
 
 		// TODO: work with verbsOfInterest-File
-		List<String> verbsOfInterest = null;
+		List<String> verbsOfInterest = readVerbsOfInterest(VERBSOFINTERESTFILE);
 		DepCompetenceFinder finder = new DepCompetenceFinder(verbsOfInterest);
 		Map<Paragraph, List<SlotFiller>> allResults = new HashMap<Paragraph, List<SlotFiller>>();
 
@@ -54,8 +60,70 @@ public class DepCompetenceFinderWorkflow {
 		evaluate(allResults);
 	}
 
-	private static void evaluate(Map<Paragraph, List<SlotFiller>> allResults) {
+	private static List<String> readVerbsOfInterest(String fileName) {
+		List<String> verbs = null;
+		
+		File file = new File(fileName);
+		BufferedReader r = null;
+		try {
+			verbs = new ArrayList<String>();
+			r = new BufferedReader(new InputStreamReader(new FileInputStream(
+					file)));
+			String line;
+			while ((line = r.readLine()) != null) {
+				if(line.split("\\s").length != 1) {
+					throw new IllegalArgumentException("File has wrong format");
+				}
+				verbs.add(line.trim());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				r.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return verbs;
+	}
 
+	private static void evaluate(Map<Paragraph, List<SlotFiller>> allResults) {
+		IETrainingDataGenerator gen = new IETrainingDataGenerator(new File(
+				IE_TRAININGDATAFILE), Class.COMPETENCE);
+
+		Map<Paragraph, List<SlotFiller>> manuallyLabeled;
+		try {
+			manuallyLabeled = gen.getTrainingData();
+			for (Paragraph par : allResults.keySet()) {
+				if (manuallyLabeled.containsKey(par)) {
+					List<SlotFiller> result = allResults.get(par);
+					List<SlotFiller> gold = manuallyLabeled.get(par);
+					compare(result, gold);
+					// ...
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void compare(List<SlotFiller> result, List<SlotFiller> gold) {
+		for (SlotFiller goldFiller : gold) {
+			for (SlotFiller resFiller : result) {
+				if (resFiller.getContent().contains(goldFiller.getContent())) {
+					// logger.info("Gold contained in result");
+					System.out.println(resFiller.getContent() + "\t|\t"
+							+ goldFiller.getContent());
+				} else if (goldFiller.getContent().contains(
+						resFiller.getContent())) {
+					// logger.info("Result contained in gold");
+					System.out.println(resFiller.getContent() + "\t|\t"
+							+ goldFiller.getContent());
+				}
+			}
+		}
 	}
 
 	private static void createParsedParagraphsFile(File parsedParagraphsFile) {
